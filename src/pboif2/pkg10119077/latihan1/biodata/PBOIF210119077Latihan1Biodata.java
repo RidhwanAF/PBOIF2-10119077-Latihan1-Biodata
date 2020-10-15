fe0d28d1949d373ab066d67782e001a16b5e99f7
RAF <RAF@ROG>
2020-10-15 14:52:01 +0700

    upload

---
+++ src/pboif2/pkg10119077/latihan1/biodata/PBOIF210119077Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119077.latihan1.biodata;
+
+/**
+ *
+ * @author RAF
+ * Nama         : Ridhwan Anwar Fauzan
+ * Kelas        : IF-2 
+ * NIM          : 10119077
+ * Deskripsi    : Menampilankan Biodata
+ */
+public class PBOIF210119077Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------------------------------");
+        System.out.println("NAMA\t\t: Ridhwan Anwar Fauzan");
+        System.out.println("ALAMAT\t\t: Pasir Impun");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 08 November 2001");
+        System.out.println("---------------------------------------------");
+        System.out.println("HOBI\t\t: Olahraga");
+        System.out.println("MAKES\t\t: INDOMIE goreng");
+        
+    }
+    
+}
